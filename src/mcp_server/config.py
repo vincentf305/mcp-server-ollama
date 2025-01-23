5a5ec4239a27d813f627ce1e5c38ba0201749323
vincentf305 <vincentfaller@gmail.com>
2025-01-23 17:48:51 -0500

    Add configuration module

---
+++ src/mcp_server/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ollama_base_url: str = "http://localhost:11434"
+    model_name: str = "llama2"
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
